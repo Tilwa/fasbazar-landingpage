98e3b74849d4a6a3be2fa985285cd19f52a5b058
Shahrukh Altaf <shahrukhaltaf123@gmail.com>
2022-03-30 08:52:04 +0400

    app link deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import LandingTop from "./components/landingTop/LandingTop";
 import Cards from "./components/cards/Cards";
-import "./app.css";
+
 import Timer from "./components/timer/Timer";
 
 function App() {
